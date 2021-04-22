94999c9a61597c900d17a9fb7eb8680d420fb6c6
crouchr <richard.crouch100@gmail.com>
2021-04-22 19:00:40 +0100

    Fixed CICD script

--- cicd/build_docker_image.sh
+++ cicd/build_docker_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ..
-docker build --no-cache -t cicd:netmond .
-docker tag cicd:netmond registry:5000/netmond:$VERSION
-docker push registry:5000/netmond:$VERSION
-docker rmi cicd:netmond
-
+docker build --no-cache -t cicd:netmon .
+docker tag cicd:netmon registry:5000/netmon:$VERSION
+docker push registry:5000/netmon:$VERSION
+docker rmi cicd:netmon
